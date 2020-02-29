67a27b400a4af6a8df57800f3b960d137c9a2995
caique oliveira <caique_oliveira@outlook.com>
2020-02-29 14:37:39 -0300

    commits-card1

--- test-drogaraia/src/components/layouts/Cards.js
+++ test-drogaraia/src/components/layouts/Cards.js
@@ -8,15 +8,15 @@ import Mobile from '../../imagens/mobile.png'
 function Cards(){
   function handleClick(){
 
-      var txt = document.getElementsByClassName('card-text').innerHTML;
+      var txt = document.getElementById('text-card').innerHTML;
       var txt2 = txt.substr(0,100)+'...';
-      document.getElementsByClassName('card-text').innerHTML = txt2;
+      document.getElementById('text-card').innerHTML = txt2;
    
      if(txt<txt2 ){
-       document.getElementsByClassName('card-text').innerHTML = txt;
+       document.getElementById('text-card').innerHTML = txt;
        document.getElementById('btn1').innerHTML = 'Mostrar Menos'; 
      }else{
-        document.getElementsByClassName('card-text').innerHTML = txt2;
+        document.getElementById('text-card').innerHTML = txt2;
         document.getElementById('btn1').innerHTML = 'Mostrar Mais'; 
      }
    
@@ -24,10 +24,10 @@ function Cards(){
    function Mostrar(){
    
       if(document.getElementById('btn1').innerHTML == 'Mostrar Mais'){
-        document.getElementsByClassName('card-text').innerHTML = txt;
+        document.getElementById('text-card').innerHTML = txt;
         document.getElementById('btn1').innerHTML = 'Mostrar Menos'; 
       }else{
-        document.getElementsByClassName('card-text').innerHTML = txt2;
+        document.getElementById('text-card').innerHTML = txt2;
         document.getElementById('btn1').innerHTML = 'Mostrar Mais'; 
       }
    
@@ -41,7 +41,7 @@ function Cards(){
           <img className="card-img-top icon-desk" src={Desktop}/>
           <div className="card-body">
             <h5 className="card-title">Site Responsivo Desktop</h5>
-            <p className="card-text">Quando pressionado o botão <strong>Leia mais...</strong> &nbsp;o
+            <p id="text-card" className="card-text">Quando pressionado o botão <strong>Leia mais...</strong> &nbsp;o
 restante da informação deverá aparecer e, scroll down.</p>
             <a href="#" id="btn1" className="btn btn-primary" onClick={handleClick}>Leia mais</a>
           </div>
@@ -50,7 +50,7 @@ restante da informação deverá aparecer e, scroll down.</p>
           <img className="card-img-top icon-tablet" src={Tablet}/>
           <div className="card-body">
             <h5 className="card-title">Site Responsivo Desktop</h5>
-            <p className="card-text">Quando pressionado o botão <strong>Leia mais...</strong> &nbsp;o
+            <p id="text-card" className="card-text">Quando pressionado o botão <strong>Leia mais...</strong> &nbsp;o
 restante da informação deverá aparecer e, scroll down.</p>
             <a href="#" id="btn2" className="btn btn-primary btn2">Leia mais</a>
           </div>
