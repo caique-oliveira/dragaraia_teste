83c334718f900645e3d6c2f1cdce938a22f899e6
caique oliveira <caique_oliveira@outlook.com>
2020-03-01 16:50:43 -0300

    add-themeswitcher

--- test-drogaraia/src/App.js
+++ test-drogaraia/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import { ThemeProvider } from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import './App.css';
 import Navbar from './components/layouts/Navbar';
@@ -6,13 +7,37 @@ import styled from 'styled-components';
 import Footer from './components/layouts/Footer';
 import Cards from './components/layouts/Cards';
 import {NavbarSection, Logo, LogoText, UlList, ListItem, Anchor} from './components/layouts/style.js'
+import TodoList from './components/TodoList';
+import ThemeSwitcher from './components/ThemeSwitcher';
+import * as themes from './styles/themes';
+import ThemeContext from './styles/themes/context';
+
 
 class App extends Component {
+  state ={
+    themes: themes.dark,
+  };
+
+  toggleTheme = () => {
+    this.setState({ theme: this.state.theme == themes.dark ? themes.light: themes.dark });
+  };
+
   render() {
     return (
       <div>
         <Navbar />
         <Cards />
+        <div>
+          <ThemeContext.Provider value={this.state}>
+          <ThemeSwitcher toggleTheme={this.toggleTheme} />
+          <ThemeContext.consumer>
+            {theme => <ThemeProvider theme={theme}>
+                        <TodoList />
+                      </ThemeProvider> 
+            }
+          </ThemeContext.consumer>
+          </ThemeContext.Provider>
+        </div>
         <Footer />
       </div>
     );
--- test-drogaraia/src/components/layouts/Cards.js
+++ test-drogaraia/src/components/layouts/Cards.js
@@ -3,35 +3,18 @@ import styled from 'styled-components';
 import Desktop from '../../imagens/desktop.png'
 import  Tablet from '../../imagens/tablet.png'
 import Mobile from '../../imagens/mobile.png'
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import PerfectScrollbar from 'react-perfect-scrollbar'
 
 
 function Cards(){
-  function handleClick(){
-
-      var txt = document.getElementById('text-card').innerHTML;
-      var txt2 = txt.substr(0,100)+'...';
-      document.getElementById('text-card').innerHTML = txt2;
-   
-     if(txt<txt2 ){
-       document.getElementById('text-card').innerHTML = txt;
-       document.getElementById('btn1').innerHTML = 'Mostrar Menos'; 
-     }else{
-        document.getElementById('text-card').innerHTML = txt2;
-        document.getElementById('btn1').innerHTML = 'Mostrar Mais'; 
-     }
-   
-       
-   function Mostrar(){
-   
-      if(document.getElementById('btn1').innerHTML == 'Mostrar Mais'){
-        document.getElementById('text-card').innerHTML = txt;
-        document.getElementById('btn1').innerHTML = 'Mostrar Menos'; 
-      }else{
-        document.getElementById('text-card').innerHTML = txt2;
-        document.getElementById('btn1').innerHTML = 'Mostrar Mais'; 
-      }
-   
-   }
+  function handleClick(e) {
+    if (e.target.id == "btn1") {
+      document.getElementById("showmore-card1").classList.toggle("hide");
+    }
+    if (e.target.id == "btn2") {
+      document.getElementById("showmore-card2").classList.toggle("hide");
+    }
   }
   
   return(
@@ -40,19 +23,37 @@ function Cards(){
         <div className="card" style = {{width: "20em"}}>
           <img className="card-img-top icon-desk" src={Desktop}/>
           <div className="card-body">
-            <h5 className="card-title">Site Responsivo Desktop</h5>
+            <h5 className="card-title">SITE RESPONSIVO Desktop</h5>
+            <PerfectScrollbar>
             <p id="text-card" className="card-text">Quando pressionado o botão <strong>Leia mais...</strong> &nbsp;o
-restante da informação deverá aparecer e, scroll down.  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+restante da informação deverá aparecer e, scroll down. 
+                <span id="showmore-card1" className="hide">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatibus iusto nesciunt quo veritatis incidunt labore,
+                praesentium nisi voluptate in perspiciatis reiciendis sapiente
+                voluptas aut odit aspernatur nostrum deleniti sint veniam!
+              </span>
+              </p>
+              </PerfectScrollbar>
             <a href="#" id="btn1" className="btn btn-primary" onClick={handleClick}>Leia mais</a>
           </div>
         </div>
         <div className="card" style = {{width: "20em"}}>
           <img className="card-img-top icon-tablet" src={Tablet}/>
           <div className="card-body">
-            <h5 className="card-title">Site Responsivo Desktop</h5>
+            <h5 className="card-title">SITE RESPONSIVO TABLET</h5>
+            <PerfectScrollbar>
             <p id="text-card" className="card-text">Quando pressionado o botão <strong>Leia mais...</strong> &nbsp;o
-restante da informação deverá aparecer e, scroll down.</p>
-            <a href="#" id="btn2" className="btn btn-primary btn2">Leia mais</a>
+restante da informação deverá aparecer e, scroll down. 
+<span id="showmore-card2" className="hide">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatibus iusto nesciunt quo veritatis incidunt labore,
+                praesentium nisi voluptate in perspiciatis reiciendis sapiente
+                voluptas aut odit aspernatur nostrum deleniti sint veniam!
+              </span>
+              </p>
+              </PerfectScrollbar>
+            <a href="#" id="btn2" className="btn btn-primary btn2" onClick={handleClick}>Leia mais</a>
           </div>
         </div>
         
@@ -60,7 +61,7 @@ restante da informação deverá aparecer e, scroll down.</p>
           <img className="card-img-top icon-mobile" src={Mobile}/>
           <div className="card-body">
             <h5 className="card-title">Site Responsivo Desktop</h5>
-            <p className="card-text">Quando pressioonado o botão <strong>alterar tema</strong>&nbsp; 
+            <p className="card-text">Quando pressionado o botão <strong>alterar tema</strong>&nbsp; 
               modifique o tema do site para blackfriday a seu
               gosto.</p>
             <a href="#" id="btn3" className="btn btn-primary btn3">alterar tema</a>
